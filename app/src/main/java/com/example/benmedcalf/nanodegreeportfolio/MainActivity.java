1b3cebb29e41a0d308aa3290b4a6817e6f4ff925
Ben Medcalf <ben.medcalf@gmail.com>
2016-04-20 16:11:54 -0400

    starting up this project

---
+++ app/src/main/java/com/example/benmedcalf/nanodegreeportfolio/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.benmedcalf.nanodegreeportfolio;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ben.medcalf on 4/20/16.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
